5f744d4f95211a2a5be8728558acb90423c8124c
madhumithathulasiraman <madhumitha0073@gmail.com>
2026-02-05 16:28:34 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,42 @@
+#Addition
+a=input("a=")
+b=input("b=")
+print("a+b=",a+b)
+#User input
+name=input("Enter your name:")
+age=input("Enter your age:")
+print("My Name is",name)
+print("My Age is",age)
+#Arithemetic Operation
+a=10
+b=20
+c=a+b
+c=a-b
+c=a*b
+c=a%b
+print("c=",a+b)
+print("c=",a-b)
+print("c=",a*b)
+print("c=",a%b)
+#user input
+a=int(input())
+b=int(input())
+c=a+b
+print("a+b=",c)
+#Personal Details From User
+
+print("-----Student Registration-----")
+name=input("Enter Your Name:")
+age=input("Enter Your Age:")
+course=input("Enter Your Course: ")
+mail=input("Enter Your Mail:")
+phone=input("Enter Your Phone Number:")
+
+print("----Registration Successfully----")
+print("Student Name :",name)
+print("Age          :",age)
+print("Course       :",course)
+print("E-mail       :",mail)
+print("phone        :",phone)
+
+
