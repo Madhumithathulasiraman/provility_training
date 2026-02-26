90254dc9427100fd61eb47da37ebcdf4f49c4b1a
madhumithathulasiraman <madhumitha0073@gmail.com>
2026-02-26 16:54:10 +0530

    first commit

--- 11th_SB_chapter9/exercise91/exercise9112/step_1.py
+++ 11th_SB_chapter9/exercise91/exercise9112/step_1.py
@@ -12,18 +12,14 @@ class ExerciseAnswer9112(Scene):
         self.camera.background_color = WHITE
 
         
-        axes=Axes(x_range=[-2.2,8,1],y_range=[-2.5,3,1],x_length=12,y_length=6,axis_config={"color":BLACK,"stroke_width":4,"include_ticks":True})
-        labels_right=VGroup(*[MathTex(str(i),font_size=40,color=BLACK).next_to(axes.c2p(i,0),DOWN*0.5+LEFT*0.1)for i in range(1,8)])
-        for tick in axes.y_axis.get_tick_marks():
-            if tick.get_center()[1]<0:   
-                tick.set_opacity(0)
-        for tick in axes.y_axis.get_tick_marks():
-            if tick.get_center()[0]<2:   
-                tick.set_opacity(0)
-        for tick in axes.x_axis.get_tick_marks():
-            if tick.get_center()[0]<-4:   
-                tick.set_opacity(0)
-
+        axes=Axes(x_range=[-2.5,8,1],y_range=[-2.5,3,1],x_length=12,y_length=6,axis_config={"color":BLACK,"stroke_width":4,"include_ticks":True}).set_z_index(10)
+        labels_right=VGroup(*[MathTex(str(i),font_size=35,color=BLACK).next_to(axes.c2p(i,0),DOWN*0.5+LEFT*0.1)for i in range(1,8)])
+        labels_left=MathTex("-1",color=BLACK,font_size=35).next_to(axes.c2p(-1,0),DOWN*0.5)
+        labels_left2=MathTex("-2",color=BLACK,font_size=35).next_to(axes.c2p(-2,0),DOWN*0.5)
+        labels_up=MathTex("1",color=BLACK,font_size=35).next_to(axes.c2p(0,1),DOWN*0.4+LEFT*0.5)
+        labels_up2=MathTex("2",color=BLACK,font_size=35).next_to(axes.c2p(0,2),DOWN*0.4+LEFT*0.5)
+        labels_down=MathTex("-1",color=BLACK,font_size=35).next_to(axes.c2p(0,-1),UP*0.5+LEFT*0.5)
+        labels_down2=MathTex("-2",color=BLACK,font_size=35).next_to(axes.c2p(0,-2),UP*0.9+LEFT*0.5)
         axes.x_axis.remove(axes.x_axis.tip) if hasattr(axes.x_axis, "tip") else None
         axes.y_axis.remove(axes.y_axis.tip) if hasattr(axes.y_axis, "tip") else None
 
@@ -40,35 +36,35 @@ class ExerciseAnswer9112(Scene):
         axis_label_2=MathTex("y",font_size=55,color=BLACK).next_to(axes.c2p(0,3),RIGHT*1)
         axis_label=MathTex("y'",font_size=55,color=BLACK).next_to(axes.c2p(0,-3),UP*1.5+RIGHT)
         
-        line_1=Line(axes.c2p(5,1.2),axes.c2p(8,1.2),color="#008000",stroke_width=6).scale(0.8)
-        line_2=Line(axes.c2p(-2,-1),axes.c2p(5,-1),color="#008000",stroke_width=6).scale(0.5).shift(RIGHT*1.6)
-        hole=Circle(radius=0.1,color="#008000",stroke_width=6)
-        hole.move_to(axes.c2p(5,0)).shift(DOWN*1+LEFT*0.3)
-        hole_2=Circle(radius=0.1,color="#008000",stroke_width=6)
-        hole_2.move_to(axes.c2p(5,1)).shift(UP*0.2+RIGHT*0.3)
+        line_1=Line(axes.c2p(5,1.2),axes.c2p(8,1.2),color="#008000",stroke_width=4).scale(0.8).shift(LEFT*0.2+DOWN*0.5)
+        line_2=Line(axes.c2p(-2,-1),axes.c2p(5,-1),color="#008000",stroke_width=4).scale(0.8).shift(RIGHT*0.7+UP*0.3)
+        hole=Circle(radius=0.1,color="#008000",stroke_width=4)
+        hole.move_to(axes.c2p(5,0)).shift(DOWN*0.74+LEFT*0.1)
+        hole_2=Circle(radius=0.1,color="#008000",stroke_width=4)
+        hole_2.move_to(axes.c2p(5,1)).shift(DOWN*0.3+RIGHT*0.1)
         hole.set_fill(WHITE,opacity=2)
         hole_2.set_fill(WHITE,opacity=2)
-        hole_3=Circle(radius=0.20,color="#FF0000",stroke_width=6)
+        hole_3=Circle(radius=0.20,color="#FF0000",stroke_width=4)
         hole_3.move_to(axes.c2p(5,0)).shift(DOWN*0.12+RIGHT*0.07)
 
-        self.add(axes,labels_right,axis_label_1,axis_label2,axis_label_2,axis_label,line_1,line_2,hole,hole_2,hole_3)
+        self.add(axes,labels_right,labels_left,labels_left2,labels_up,labels_up2,labels_down,labels_down2,axis_label_1,axis_label2,axis_label_2,axis_label,line_1,line_2,hole,hole_2,hole_3)
 
-        dashed_line=DashedLine(start=axes.c2p(5,-2),end=axes.c2p(5,2),color="#834333",stroke_width=6,stroke_opacity=0.6)
+        dashed_line=DashedLine(start=axes.c2p(5,-0.6),end=axes.c2p(5,0.6),color="#834333",stroke_width=6,stroke_opacity=0.6)
         self.add(dashed_line,)
         
-        arrow_1=Arrow(LEFT*3,RIGHT*3,color="#800080",stroke_width=4,tip_length=0.25).shift(DOWN*1+LEFT*0.1).scale(0.8)
-        arrow_2=Arrow(RIGHT*3,LEFT*3,color="#800080",stroke_width=4,tip_length=0.25).shift(DOWN*1+RIGHT*4.2).scale(0.6)
-        arrow_3=Arrow(DOWN*2,UP*3,color="#800080",stroke_width=3,tip_length=0.25).shift(DOWN*2.1+RIGHT*2.4).scale(0.2)
-        label_1=MathTex("5^{-}",color=BLACK,font_size=30)
-        label_2=MathTex("5^{+}",color=BLACK,font_size=30)
+        arrow_1=Arrow(LEFT*3,RIGHT*3,color="#800080",stroke_width=4,tip_length=0.15).shift(DOWN*1+RIGHT*1.9).scale(0.1)
+        arrow_2=Arrow(RIGHT*3,LEFT*3,color="#800080",stroke_width=4,tip_length=0.15).shift(DOWN*-0.5+RIGHT*2.9).scale(0.1)
+        
+        label_1=MathTex("5^{+}",color=BLACK,font_size=15)
+        label_2=MathTex("5^{-}",color=BLACK,font_size=15)
 
-        label_1.next_to(arrow_1.get_end(),LEFT*1.5+DOWN*0.6)
-        label_2.next_to(arrow_2.get_end(),RIGHT*1.5+DOWN*0.6)
-        self.add(arrow_1,arrow_2,arrow_3,label_1,label_2)
+        label_1.next_to(arrow_1.get_end(),LEFT*-0.01+UP*0.3)
+        label_2.next_to(arrow_2.get_end(),RIGHT*2.5+DOWN*0.01+UP*0.1)
+        self.add(arrow_1,arrow_2,label_1,label_2)
 
-        zero_label = MathTex("0", color=BLACK)
+        zero_label = MathTex("0", color=BLACK,font_size=35)
 
         zero_label.next_to(axes.c2p(0.2,0), DOWN*0.2+LEFT*1.7)
-        label=MathTex("y=|x-5|/x-5",font_size=35,color=BLACK).shift(UP*1.3+RIGHT*4)
+        label=MathTex(r"f(x) = \frac{|x - 5|}{x - 5}",font_size=35,color=BLACK).shift(UP*2+RIGHT*1)
 
         self.add(zero_label,label)
--- 11th_SB_chapter9/exercise91/exercise9113/step_1.py
+++ 11th_SB_chapter9/exercise91/exercise9113/step_1.py
@@ -5,7 +5,7 @@ config.background_color = WHITE
 
 class ExerciseAnswer9113(Scene):
     def construct(self):
-        axes=Axes(x_range=[-1.5,2.5,1],y_range=[-1.5,1.5,1],axis_config={"color":BLACK},tips=True,x_axis_config={"include_numbers":False,"include_ticks": False,},y_axis_config={"include_numbers":False},).set_z_index(1)
+        axes=Axes(x_range=[-1.9,2.5,1],y_range=[-1.5,1.5,1],axis_config={"color":BLACK},tips=True,x_axis_config={"include_numbers":False,"include_ticks": False,},y_axis_config={"include_numbers":False},).set_z_index(1)
         axes.x_axis.set_stroke(width=4)
         axes.y_axis.set_stroke(width=4)
         axes.x_axis.add_tip(at_start=True)
@@ -16,28 +16,51 @@ class ExerciseAnswer9113(Scene):
         
 
         labels=axes.get_axis_labels(MathTex("x",color=BLACK),MathTex("y",color=BLACK))
-        axis_label1=MathTex("x'",font_size=55,color=BLACK).next_to(axes.c2p(-2,0),RIGHT*4.5+UP*0.5)
+        axis_label1=MathTex("x'",font_size=55,color=BLACK).next_to(axes.c2p(-2.5,0),RIGHT*4.5+UP*0.5)
         axis_label2=MathTex("y'",font_size=55,color=BLACK).next_to(axes.c2p(0,-2),UP*2.5+RIGHT)
         labels_left=VGroup(MathTex("-1",font_size=40,color=BLACK).next_to(axes.c2p(-1.1, 0), DOWN*0.5),)
         y_labels=VGroup(MathTex("1",font_size=40,color=BLACK).next_to(axes.c2p(0,1),LEFT*0.5),MathTex("-1",font_size=40,color=BLACK).next_to(axes.c2p(0,-0.9),LEFT*0.6),)
         labels_right=VGroup(MathTex("1",font_size=40,color=BLACK).next_to(axes.c2p(1, 0), DOWN*0.8+LEFT*0.2),MathTex("2",font_size=40,color=BLACK).next_to(axes.c2p(2.03, 0), DOWN*0.5),)
         dot=Dot(axes.coords_to_point(1,0),color="#483AE4").set_z_index(10)
         
-        # Curve
-        curve = axes.plot(lambda x:np.sin(np.pi * x),x_range=[-1,2.5],color="#F5276C",stroke_width=4).set_z_index(1)
 
-        self.add(axes,tick_1,tick_2,tick_3,labels,axis_label1,axis_label2,labels_left,y_labels,labels_right,dot,curve)
+        curve = axes.plot(lambda x:np.sin(np.pi * x),x_range=[-1.6,2.5],color="#F5276C",stroke_width=4).set_z_index(1)
+        self.add(curve)
+        x_end = 2.5
+        dx = 0.3
 
-        arrow_1=Arrow(LEFT*3,RIGHT*3,color="#6A0B37",stroke_width=4,tip_length=0.25).shift(DOWN*0.7+LEFT).scale(0.8)
-        arrow_2=Arrow(RIGHT*3,LEFT*3,color="#6A0B37",stroke_width=4,tip_length=0.25).shift(DOWN*0.7+RIGHT*3.7).scale(0.8)
-        arrow_3=Arrow(DOWN*2,UP*3,color="#6A0B37",stroke_width=3,tip_length=0.25).shift(DOWN*1.8+RIGHT*1.4).scale(0.2)
+        p1 = axes.c2p(x_end - dx, np.sin(np.pi*(x_end - dx)))
+        p2 = axes.c2p(x_end, np.sin(np.pi*x_end))
+
+        end_tip = Arrow(p1, p2, buff=0,
+                        color="#F5276C",
+                        stroke_width=0).shift(UP*0.13+RIGHT*0.05)
+        end_tip.tip.rotate(-PI/6, about_point=end_tip.tip.get_center())
+
+        
+        x_start = -1.65
+        dx=0.4
+        p3 = axes.c2p(x_start + dx, np.sin(np.pi*(x_start + dx)))
+        p4 = axes.c2p(x_start, np.sin(np.pi*x_start))
+
+        start_tip = Arrow(p3, p4, buff=0,
+                          color="#F5276C",
+                          stroke_width=0).shift(UP*0.20+LEFT*0.01)
+        start_tip.tip.rotate(PI, about_point=start_tip.tip.get_center())
+        self.add(end_tip, start_tip)
+        
+        self.add(axes,tick_1,tick_2,tick_3,labels,axis_label1,axis_label2,labels_left,y_labels,labels_right,dot)
+
+        arrow_1=Arrow(LEFT*3,RIGHT*3,color="#6A0B37",stroke_width=4,tip_length=0.25).shift(DOWN*0.7+LEFT*0.9).scale(0.95)
+        arrow_2=Arrow(RIGHT*3,LEFT*3,color="#6A0B37",stroke_width=4,tip_length=0.25).shift(DOWN*0.7+RIGHT*4).scale(0.8)
+    
         label_1=MathTex("1^{-}",color=BLACK,font_size=30)
         label_2=MathTex("1^{+}",color=BLACK,font_size=30)
-        label_3=MathTex("y=\\sin\\pi x",color=BLACK,font_size=35).next_to(axes.c2p(0.5,1),UP*0.2)
+        label_3=MathTex("f(x)=\\sin\\pi x",color=BLACK,font_size=35).next_to(axes.c2p(0.5,1),UP*0.2)
 
         label_1.next_to(arrow_1.get_end(),LEFT*1.5+DOWN*0.6)
         label_2.next_to(arrow_2.get_end(),RIGHT*1.5+DOWN*0.6)
-        self.add(arrow_1,arrow_2,arrow_3,label_1,label_2,label_3)
+        self.add(arrow_1,arrow_2,label_1,label_2,label_3)
 
         hole=Circle(radius=0.20,color="#D5E33B",stroke_width=6)
         hole.move_to(axes.c2p(1,0)).shift(DOWN*0.33+LEFT*0.12)
