deefaec6949e31e463e538110000be168449ddee
madhumithathulasiraman <madhumitha0073@gmail.com>
2026-03-06 16:12:44 +0530

    first commit

--- 11th_SB_chapter9/exercise95/exercise9515/step_1.py
+++ 11th_SB_chapter9/exercise95/exercise9515/step_1.py
@@ -1,11 +1,11 @@
 from manim import *
 
-class DiscontinuityDiagram(Scene):
+class ExerciseAnswer9515a(Scene):
     def construct(self):
         self.camera.background_color = WHITE
 
        
-        axes = Axes(x_range=[-3, 5, 1],y_range=[-3, 10, 1],x_length=8,y_length=6,axis_config={"color": GREEN,"stroke_width":4,"include_ticks":False},tips=False)
+        axes = Axes(x_range=[-3, 5, 1],y_range=[-3, 10, 1],x_length=8,y_length=6,axis_config={"color":BLACK,"stroke_width":3.5,"include_ticks":False},tips=False)
         axes.x_axis.add_tip()
         axes.x_axis.add_tip(at_start=True)
         axes.y_axis.add_tip()
@@ -14,12 +14,12 @@ class DiscontinuityDiagram(Scene):
 
       
         x0 = 2
-        left_curve = axes.plot(lambda x: 3 - 0.1*(x+1)**2,x_range=[-3, x0],color="#2A27F5",stroke_width=5).shift(UP*1.8).set_z_index(10)
-        right_curve = axes.plot(lambda x: 1.3*(x-x0)**2 + 2.2,x_range=[x0, 4],color="#F58727",stroke_width=4)
+        left_curve = axes.plot(lambda x: 3 - 0.1*(x+1)**2,x_range=[-3, x0],color=PINK,stroke_width=5).shift(UP*1.8).set_z_index(10)
+        right_curve = axes.plot(lambda x: 1.3*(x-x0)**2 + 2.2,x_range=[x0, 4],color=BLUE,stroke_width=5)
         open_circle = Circle(radius=0.12, color=BLACK, stroke_width=4)
         open_circle.set_fill(WHITE, opacity=1)
         open_circle.move_to(axes.c2p(x0,2.2))
-        filled_dot = Dot(axes.c2p(x0, 6),color=BLUE).scale(1.2).set_z_index(10)
+        filled_dot = Dot(axes.c2p(x0, 6),color=RED).scale(1.2).set_z_index(10)
         v_line = Line(axes.c2p(x0, 0),axes.c2p(x0, 6),color="#8B1A1A",stroke_width=5)
 
         
@@ -60,16 +60,16 @@ class DiscontinuityDiagram(Scene):
         brace = BraceBetweenPoints(p1, p2, direction=RIGHT,color=BLACK).shift(UP*0.8)
         self.add(axes, brace)
 
-        arrow = Arrow(start=LEFT * 2,end=RIGHT * 2,color="#2A27F5",buff=0).scale(0.8).shift(LEFT*1+UP*0.7)
-        arrow2= Arrow(start=RIGHT * 2,end=LEFT * 2,color="#F58727",buff=0).scale(0.65).shift(RIGHT*2.6+DOWN*1)
+        arrow = Arrow(start=LEFT * 2,end=RIGHT * 2,color=PINK,buff=0).scale(0.8).shift(LEFT*1+UP*0.7)
+        arrow2= Arrow(start=RIGHT * 2,end=LEFT * 2,color=BLUE,buff=0).scale(0.65).shift(RIGHT*2.6+DOWN*1)
         label = MathTex("x_0^{-}", color=BLACK)
         label.next_to(arrow.get_center(),RIGHT*1.5+DOWN*0.6).scale(0.8)
         label1 = MathTex("x_0^{+}", color=BLACK)
         label1.next_to(arrow2.get_center(),DOWN*0.1).scale(0.8)
         self.add(arrow,arrow2,label, label1)
         
-        eq1 = MathTex(r"\text{There exists a gap between } x_0^{+} \text{ and } x_0^{-}", color=BLACK).scale(0.5)
-        eq2 = MathTex(r"(ie)\lim_{x \to x_0} f(x) \neq f(x_0)", color=BLACK).scale(0.5)
+        eq1 = MathTex(r"\text{There exist a gap between } x_0^{+} \text{ and } x_0^{-}", color=BLACK).scale(0.5)
+        eq2 = MathTex(r"(ie)\lim_{x \to x_0^-} f(x) = f(x_0) \neq \lim_{x \to x_0^+} f(x)", color=BLACK).scale(0.5)
         eq3=MathTex(r"\text{Not continuous at } x = x_0",color=BLACK).scale(0.5)
         group = VGroup(eq1, eq2,eq3).arrange(DOWN, aligned_edge=LEFT, buff=0.2)
         box = SurroundingRectangle(group, color=BLACK, buff=0.1)
